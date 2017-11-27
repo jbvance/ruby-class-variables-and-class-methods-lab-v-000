28ea55addcf9c29f50fb69c0e1a8790eaa129e71
Jason Vance <jbvance@gmail.com>
2017-11-27 21:58:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,16 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
+  # Class level variables
   @@count = 0
   @@genres = []
+  @@artists = []
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    
+
     @@genres << genre
     @@count += 1
   end
@@ -20,10 +22,18 @@ class Song
 
   def genres
     returnArr = []
-    @@genres.each do |genre|  
-      tmp >> genre if !@@genres.include?(genre)
-    end    
+    @@genres.each do |genre|
+      returnArr >> genre if !@@genres.include?(genre)
+    end
     returnArr
   end
-  
+
+  def artists
+    returnArr = []
+    @@artists.each do |artist|
+      returnArr >> artist if !@@artists.include?(artist)
+    end
+    returnArr
+  end
+
 end
